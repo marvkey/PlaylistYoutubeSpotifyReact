92882114f4cc321f8c034090ed8007fa500527f2
marv nwadike <marvnwadike1@gmail.com>
2022-03-19 21:46:54 -0700

    images

--- client/src/pages/View.js
+++ client/src/pages/View.js
@@ -14,12 +14,14 @@ const getReturnedParamsFromSpotifyAuth = (hash) => {
 
   return paramsSplitUp;
 }; 
+var spotifyImages =[]
+
 export default function View(){
 
       
     const [spotifyData,setSpotifyData] = useState({})
     const [spotifyAccesToken,setSpotifyAccessToken] = useState('');
-    var spotifyImages =[]
+    const [spotifyImageser,setSpotifyImages] = useState([]);
     useEffect(() => {
         if (window.location.hash) {
           const { access_token, expires_in, token_type } =
@@ -41,21 +43,51 @@ export default function View(){
           })
           .then((response) => {
             setSpotifyData(response.data);
+            //if(spotifyData.items != null ){
+            //    spotifyData.items.map((item) => spotifyImages.push(item.images.url))
+           // }
           })
           .catch((error) => {
             console.log(error);
           });
       };
+
+      const handleGetImages=() =>{
+        handleGetPlaylists();
+        //if(spotifyImages.items  == null)return;
+        //0oRUQemdMM2IPVwmiDHDdJ
+        spotifyData.items.map((item) =>
+        axios.get("https://api.spotify.com/v1/playlists/"+item.id+"/images",{
+          headers: {
+            Authorization: "Bearer " + spotifyAccesToken,
+          },
+        })
+        .then((response) => {
+          const value = response.data.map((item)=>  item.url)
+          spotifyImages.push(value)
+          setSpotifyImages([...spotifyImageser,{
+            value
+          }])
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+        )
+      }
+      spotifyImages.map((item)=>console.log("yeah"+item))
     //useEffect(() => {
     //    handleGetPlaylists(); 
     //});
     //if(spotifyData.items != null ){
     // spotifyData.items.map((item) => spotifyImages.push(item.images.url))
     //} 
+  //  {spotifyData?.items ? spotifyData.items.map((item) => item.images.map(imag =><img src={imag.url} width={250} height={250} />)) : null}
+
     return (
       <>
-      <button onClick={handleGetPlaylists}>PlaylistGet</button>
-       {spotifyData?.items ? spotifyData.items.map((item) => <p>{item.name}</p>) : null}
+        <button onClick={handleGetImages}>PlaylistGet</button>
+        { spotifyImages.map((item) => <p>{item}</p>)}
+      <p>{spotifyImages[0]}</p>
       </>
     )
 } 
